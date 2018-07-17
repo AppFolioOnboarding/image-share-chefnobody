af3fa162ddb78cc6eec4646d2253692911d71fa5
Aaron Connolly <michael.connolly@appfolio.com>
2018-07-16 17:25:45 -0700

    Removed old home action, updated routes, removed old tests.

--- test/controllers/application_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class ApplicationControllerTest < ActionDispatch::IntegrationTest
-  test 'should responds with Hello World' do
-    get root_url
-    assert_response :ok
-    assert_select 'h1', 'Hello World!'
-  end
-end
