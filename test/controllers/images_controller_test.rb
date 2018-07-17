1ef8f7cb7fdbb7b84893e44fc968bf42fbda00c4
Aaron Connolly <michael.connolly@appfolio.com>
2018-07-17 11:13:55 -0700

    removed scaffolding 'help'

--- test/controllers/images_controller_test.rb
+++ test/controllers/images_controller_test.rb
@@ -14,35 +14,4 @@ class ImagesControllerTest < ActionDispatch::IntegrationTest
     get new_image_url
     assert_response :success
   end
-
-  test "should create image" do
-    assert_difference('Image.count') do
-      post images_url, params: { image: { url: @image.url } }
-    end
-
-    assert_redirected_to image_url(Image.last)
-  end
-
-  test "should show image" do
-    get image_url(@image)
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get edit_image_url(@image)
-    assert_response :success
-  end
-
-  test "should update image" do
-    patch image_url(@image), params: { image: { url: @image.url } }
-    assert_redirected_to image_url(@image)
-  end
-
-  test "should destroy image" do
-    assert_difference('Image.count', -1) do
-      delete image_url(@image)
-    end
-
-    assert_redirected_to images_url
-  end
 end
