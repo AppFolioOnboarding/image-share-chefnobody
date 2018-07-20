01e810d335d24f60b89c695c0de0aa71c619d7bb
Aaron Connolly <michael.connolly@appfolio.com>
2018-07-20 13:38:33 -0700

    ac - fixed tests so they run albeit w/o any result

--- test/page_objects/images/new_page.rb
+++ test/page_objects/images/new_page.rb
@@ -1,9 +1,11 @@
 module PageObjects
   module Images
     class NewPage < PageObjects::Document
-      path #TODO
+      path "/images"
 
-      form_for :image #TODO
+      form_for :image do
+        # TO DO something here?
+      end
 
       def create_image!(url: nil, tags: nil)
         #TODO
