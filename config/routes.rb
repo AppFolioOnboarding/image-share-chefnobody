7b5d297778b5b7fa72ba2abfc86b0d8344aac2b6
Aaron Connolly <michael.connolly@appfolio.com>
2018-07-19 21:59:59 -0700

    ac - fixed strange issue where bundler completed with error about gems

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :people
   resources :images, only: [:index, :new, :show, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'images#index'
